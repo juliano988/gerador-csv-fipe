91e676cd3ebb8d416b70bb57c385646839cd06d1
Júlio Faria <julio-pinheiroo@hotmail.com>
2021-02-18 21:03:15 -0300

    Tabela atualizada

--- ListarCarros.js
+++ ListarCarros.js
@@ -1,7 +1,7 @@
 //https://deividfortuna.github.io/fipe/
 
-const marcaDesejada = 'volkswagen';
-const modeloDesejado = '';
+const marcaDesejada = 'Peugeot';
+const modeloDesejado = '306|307';
 
 // ---------------------------------------------------------------------------
 
@@ -16,7 +16,7 @@ let anosObj = {};
 async function listarCarros() {
     console.clear();
 
-    write.sync('listarCarros.txt', 'MARCA;MODELO;ANO', { newline: true });
+    // write.sync('listarCarros.txt', 'MARCA;MODELO;ANO', { newline: true });
 
     await fetch('https://parallelum.com.br/fipe/api/v1/carros/marcas')
         .then(function (res) {
